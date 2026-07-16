7a725d474eb927687c8ee3f413f647473312db62
LUMI <verguliocarmelio@gmail.com>
2026-07-15 22:51:10 -0500

    Actualizar script.js

--- script.js
+++ script.js
@@ -4,3 +4,11 @@
 
 const hamburguesa = document.querySelector(".hamburguesa");
 const menuMovil = document.querySelector(".menu-movil");
+
+// Abrir y cerrar el menú
+
+hamburguesa.addEventListener("click", () => {
+
+    menuMovil.classList.toggle("activo");
+
+});
